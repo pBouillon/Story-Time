3627222ab316105df468eef564ac1672a979b492
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-09-18 12:51:27 +0200

    Ajout du routing

--- story-time/src/app/app-routing.module.ts
+++ story-time/src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+/**
+ * @summary Reference route constants
+ */
+const enum AppRoutes {
+  // Main page
+  Index = 'index',
+}
+
+/**
+ * @summary Define all redirections and routes
+ */
+const routes: Routes = [
+  // Redirections
+  {
+    path: '',
+    redirectTo: AppRoutes.Index,
+    pathMatch: 'full',
+  }, {
+    path: '**',
+    redirectTo: AppRoutes.Index,
+    pathMatch: 'full',
+  },
+  // Routes
+  {
+    path: AppRoutes.Index,
+    component: MenuComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ],
 })
 export class AppRoutingModule { }
