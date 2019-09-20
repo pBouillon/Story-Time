d69c14c6b05b2aba925e37a629c259e78778404b
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-09-20 19:57:35 +0200

    WiP ajout d'un formulaire

--- story-time/src/app/story/writing/infos/infos.component.ts
+++ story-time/src/app/story/writing/infos/infos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { EditorService } from '../editor.service';
 
 @Component({
@@ -8,15 +10,92 @@ import { EditorService } from '../editor.service';
 })
 export class InfosComponent implements OnInit {
 
+  public readonly OVERVIEW_LENGTH = 140;
+
+  /**
+   * @summary Form containing story's data
+   */
+  public storyMetaForm: FormGroup;
+
   /**
    * Default constructor
+   * @param formBuilder FormBuilder object to gather story's details
    * @param editorService Editor toolbox
-   * @see EditorService in '../editor.service.ts'
    */
   constructor(
+    private formBuilder: FormBuilder,
     private editorService: EditorService,
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.setupForm();
+  }
+
+  /**
+   * @summary Getter for the `author` field of the form
+   * @returns The author as string
+   */
+  get author(): string {
+    return this.fetchFormProperty('author').value;
+  }
+
+  /**
+   * @summary Getter for the `overview` field of the form
+   * @returns The overview as a string
+   */
+  get overview(): string {
+    return this.fetchFormProperty('overview').value;
+  }
+
+  /**
+   * @summary Getter for the `tags` field of the form
+   * @returns An array of tags that may be empty
+   */
+  get tags(): string {
+    return this.fetchFormProperty('tags').value
+      .split(',')
+      .map(tag => tag.trim());
+  }
+
+  /**
+   * @summary Getter for the `title` field of the form
+   * @returns The title as string
+   */
+  get title(): string {
+    return this.fetchFormProperty('title').value;
+  }
+
+  /**
+   * @summary Actions to perform on form submission
+   */
+  public onSubmit(): void {
+    if (this.storyMetaForm.valid) {
+      // TODO: action on form valid
+    }
+  }
+
+  private fetchFormProperty(property: string): any {
+    return this.storyMetaForm.get(property);
+  }
+
+  /**
+   * @summary Get the current length of the story's summary
+   * @returns The current length of the story's summary
+   */
+  public getOverviewCurrentSize(): number {
+    return this.overview.length;
+  }
+
+  /**
+   * @summary Build the story's meta form
+   */
+  private setupForm(): void {
+    this.storyMetaForm = this.formBuilder.group({
+      author: ['', Validators.required],
+      overview: ['', Validators.required],
+      tags: [''],
+      title: ['', Validators.required],
+    });
+  }
 
 }
--- story-time/src/app/story/writing/writing.module.ts
+++ story-time/src/app/story/writing/writing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfosComponent } from './infos/infos.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [InfosComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class WritingModule { }
