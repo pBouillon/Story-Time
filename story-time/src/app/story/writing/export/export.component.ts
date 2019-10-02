f21347b804371740192a05c86a38b59dd034e20b
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-10-02 08:25:42 +0200

    Add story export logic

--- story-time/src/app/story/writing/export/export.component.ts
+++ story-time/src/app/story/writing/export/export.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorService } from '../editor.service';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-export',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExportComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * @summary Generated URL of the story JSON file
+   */
+  public jsonUrl: SafeUrl;
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private editorService: EditorService,
+  ) { }
 
   ngOnInit() {
+    this.jsonUrl = this.generateDownloadJsonUri();
+  }
+
+  /**
+   * @summary Generate the download URL of the story JSON file
+   * @return The assoriated URL
+   */
+  public generateDownloadJsonUri() {
+    // Fetch story data
+    const toSerialize = JSON.stringify({
+      meta: this.editorService.getCurrentStoryMeta(),
+      story: this.editorService.getCurrentStoryChapters(),
+    });
+
+    // Generate the associated URI
+    const format = 'data:text/json;charset=UTF-8,';
+    return this.sanitizer.bypassSecurityTrustUrl(format + encodeURIComponent(toSerialize));
   }
 
 }
