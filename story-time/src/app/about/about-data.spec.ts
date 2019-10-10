c4f74ff79c52ab48ea2115806c25aa8e5a5dbfb2
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-10-10 19:44:06 +0200

    Add about data class

---
+++ story-time/src/app/about/about-data.spec.ts
@@ -0,0 +1,7 @@
+import { AboutData } from './about-data';
+
+describe('AboutData', () => {
+  it('should create an instance', () => {
+    expect(new AboutData()).toBeTruthy();
+  });
+});
---
+++ story-time/src/app/about/about-data.ts
@@ -0,0 +1,19 @@
+
+/**
+ * @todo
+ * @summary
+ */
+export class AboutData {
+
+    /**
+     * @todo
+     * 
+     * @summary
+     * @param aboutText 
+     * @param imgSource 
+     */
+    constructor(
+        public aboutText: string,
+        public imgSource: string
+    ) { }
+}
